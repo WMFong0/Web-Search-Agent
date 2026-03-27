9a8e2542858565cdd13ef308d4c75c1acea0ce61
WMFong0 <wmfongyt@gmail.com>
2026-03-27 20:17:31 +0800

    Remove unused file

--- test.py
+++
@@ -1,7 +0,0 @@
-import pytest  
-from main import format_response  # Replace with actual module name  
-  
-def test_format_response():  
-    response = "BOH Bee Honey Mask, BOH Green Tea Essence, BOH Probiotic Lotion"  
-    expected_output = "BOH Bee Honey Mask"  
-    assert format_response(response) == expected_output  
